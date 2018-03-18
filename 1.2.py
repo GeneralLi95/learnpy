bcf239550c2757e142615f6a600bd59e80c38922
liyao <nudtliyao@foxmail.com>
2018-03-18 22:02:29 +0800

    1.2 Python3基本数据类型string

---
+++ 1.2.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@author:yaoli 
+@file: 1.2.py   基本数据结构  string
+@time: 2018/03/18 
+"""
+
+str = 'xiaozhupeiqi'
+print (str)                     # 输出字符串
+print (str[0:-1])               # 输出第一个到倒数第二个的所有字符
+print (str[0])                  # 输出字符串的第一个字符
+print (str[2:5])                # 输出从第三个开始到第五个的字符
+print (str[2:])                 # 输出第三个开始后的所有字符
+print (str * 2)                 # 输出字符串两次
+print (str + "aitiaonikeng")    # 连接字符串 小猪佩奇+爱跳泥坑
+print()
+
+
+
