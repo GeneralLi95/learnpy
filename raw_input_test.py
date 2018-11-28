fd3487f5d4f68771187c0bd3dd02d792e7a18367
liyao <nudtliyao@foxmail.com>
2018-11-28 09:24:11 +0800

    更新

---
+++ raw_input_test.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python  
+# -*- coding:utf-8 _*-
+""" 
+@author:yaoli 
+@file: raw_input_test.py 
+@time: 2018/11/27 
+"""
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        a = sys.argv[1]
+        b = sys.argv[2]
+    elif len(sys.argv) == 2:
+        a = sys.argv[1]
+        b = input("请输入参数b ")
+    elif len(sys.argv) == 1:
+        a = input("请输入参数a ")
+        b = input("请输入参数b ")
+
+    print(a, b)
+    print(sys.argv)
--- readme.md
+++ readme.md
@@ -5,4 +5,14 @@ All the scripts was wrote with Python3.x.
 
 本仓库用来存储我在学习Python过程中所编写的一些小程序。所有的代码都是用Python3.x写的。
 
-## 1 Basic Data Structure 基本数据类型
+1.1 Basic Data Structure 基本数据类型
+
+1.2 字符串操作
+
+parse_url 拼接url
+
+raw_input_test 在控制台输入参数，如果输入参数不足，就给出提示继续输入
+
+sklearn_iris_demo sklearn里的鸢尾花数据集的一个demo
+
+hello-pyqt pyqt的第一个代码
